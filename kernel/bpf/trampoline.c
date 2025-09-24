a9b8ffbd11714653047d40d814b92eef3165ac66
Leon Hwang <leon.hwang@linux.dev>
2025-09-24 13:29:30 +0800

    bpf, x64: Call perf_snapshot_branch_stack in trampoline
    Since PMU LBR is branch sensitive, the perf_snapshot_branch_stack
    function can capture the branch entries from trampoline entry to the call
    site in bpf program. However, these branch entries are meaningless to
    analyze the code logic of tracee.
    
    To elimiate such branch entries for tracing programs, it can read branch
    snapshot as soon as possible:
    
    1. Call perf_snapshot_branch_stack at the very start of trampoline for
       fentry programs.
    2. Call perf_snapshot_branch_stack after calling tracee immediately for
       fexit programs.
    
    As a result, the LBR will become useful even if there are many bpf
    programs before the one that requests LBR.
    
    Furthermore, more meaningful branch entries can be captured on AMD CPU,
    which has 16 LBR depth.
    
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- kernel/bpf/trampoline.c
+++ kernel/bpf/trampoline.c
@@ -414,8 +414,9 @@ static int bpf_trampoline_update(struct bpf_trampoline *tr, bool lock_direct_mut
 		goto out;
 	}
 
-	/* clear all bits except SHARE_IPMODIFY and TAIL_CALL_CTX */
-	tr->flags &= (BPF_TRAMP_F_SHARE_IPMODIFY | BPF_TRAMP_F_TAIL_CALL_CTX);
+	/* clear all bits except SHARE_IPMODIFY, TAIL_CALL_CTX, LBR_ENTRY and LBR_EXIT */
+	tr->flags &= (BPF_TRAMP_F_SHARE_IPMODIFY | BPF_TRAMP_F_TAIL_CALL_CTX |
+		      BPF_TRAMP_F_LBR_ENTRY | BPF_TRAMP_F_LBR_EXIT);
 
 	if (tlinks[BPF_TRAMP_FEXIT].nr_links ||
 	    tlinks[BPF_TRAMP_MODIFY_RETURN].nr_links) {
